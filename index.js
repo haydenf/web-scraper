a2ddf5e5d09939fcc512f144a433b54862eb23df
Hayden <hayden.fuller@gmail.com>
2021-02-13 15:18:09 +1100

    loops through pages, but overwrites each pages data. To fix: overwriting of prev data, change the way it selects what page to go to next (will change to page=${1} etc)

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const fs = require('fs');
 	const postOrSub = '2560'
 	const link = 'https://www.yellowpages.com.au/';  
 	const browser = await puppeteer.launch({ devtools: true });
-  
 	try {
 		const page = await browser.newPage();
 		await page.setViewport({ width: 1680, height: 947 })
@@ -24,34 +23,58 @@ const fs = require('fs');
 		await page.keyboard.press('Enter');
 	
 		await page.waitForTimeout(3000);
-	
-		const grabData = await page.evaluate(() => {
-			const scrapedData = {
-				header:  'Business Name, Email, Website, Phone\r\n',
-				list: []
-			}
-			const table = document.querySelectorAll('.search-contact-card')
 
-			table.forEach((el) => {
-				let name = el.querySelector('.listing-name')
-				let website = el.querySelector('.contact-url') || ' '
-				let email = el.querySelector('.contact-email') || ' '
-				let phone = el.querySelector('.click-to-call') || ' '
-				scrapedData.list.push([`"${name.innerHTML}", "${website.href || 'Not Found'}", "${email.title || 'Not Found'}", "${phone.href || 'Not Found'}"`])
-			})
-			return scrapedData
-		})
+		let pageN = 0;
+		const test = async (pageN) => {
+				const grabData = await page.evaluate(() => {
+					const scrapedData = {
+						header:  'Business Name, Email, Website, Phone\r\n',
+						list: []
+					}
+					const table = document.querySelectorAll('.search-contact-card')
+					table.forEach((el) => {
+						let name = el.querySelector('.listing-name')
+						let website = el.querySelector('.contact-url') || ' '
+						let email = el.querySelector('.contact-email') || ' '
+						let phone = el.querySelector('.click-to-call') || ' '
+						scrapedData.list.push([`"${name.innerHTML}", "${website.href || 'Not Found'}", "${email.title || 'Not Found'}", "${phone.href || 'Not Found'}"`])
+						})
+					return scrapedData
+				})
+				await page.waitForSelector('.flow-layout > .cell > .search-pagination-container > .button-pagination-container > .pagination:nth-child(2)')
+				await page.click('.flow-layout > .cell > .search-pagination-container > .button-pagination-container > .pagination:nth-child(2)')
+				await page.waitForTimeout(4000);
+				if(pageN < 5) {
+					pageN++;
+					return await test(pageN)
+				} else {
+					return grabData	
+				}
+			
+		}
+
+			const data = await test(pageN);
+			// await page.waitForSelector('.flow-layout > .cell > .search-pagination-container > .button-pagination-container > .pagination:nth-child(4)')
+			// await page.click('.flow-layout > .cell > .search-pagination-container > .button-pagination-container > .pagination:nth-child(4)')
+			// await page.waitForTimeout(6000);
+
 		
-		const generateCSV =  grabData.header + grabData.list.map(e => e.join(",")).join("\n");
 
-		fs.writeFile(`./${businessType + postOrSub + '--' + Date.now()}.csv`, generateCSV, function(err) {
+			const generateCSV =  data.header + data.list.map(e => e.join(",")).join("\n");
+
+			fs.writeFile(`./${businessType + postOrSub + '--' + Date.now()}.csv`, generateCSV, function(err) {
 			if(err) {
 				console.log(err)
+				return
 			}
 			console.log('file was saved')
 		})
 
-		console.log(grabData)
+			
+	
+
+
+
 		await page.close();
 		await browser.close();
 		} catch (error) {
@@ -59,4 +82,7 @@ const fs = require('fs');
 		await browser.close();
 		}
   })();
+
+ 
+	
 
