a86e3245c09a256ad253f06b38538f4f43bb353b
Hayden <hayden.fuller@gmail.com>
2021-02-15 16:56:23 +1100

    webscraper finish, will navigate each page and scrape each pages data successfully and create a csv

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-
 (async() => {
 	const businessType = 'trade'
 	const postCode = '2560'
 	const link = 'https://www.yellowpages.com.au/';  
 	const browser = await puppeteer.launch({ devtools: true });
 	try {
+
 		const page = await browser.newPage();
 		await page.setViewport({ width: 1680, height: 947 })
 		await page.goto(link, {
@@ -23,38 +23,41 @@ const fs = require('fs');
 		await page.keyboard.press('Enter');
 		await page.waitForTimeout(3000);
 
-		let pageN = 0;
-		const test = async (pageN) => {
-				const grabData = await page.evaluate(() => {
-					const scrapedData = {
-						header:  'Business Name, Email, Website, Phone\r\n',
-						list: []
-					}
+		let pageN = 1;
+		const scrapedData = {
+			header:  'Business Name, Email, Website, Phone\r\n',
+			list: []
+		}
+
+		const dataFetch = async (pageN, scrapedData) => {
+				const eachPage = await page.evaluate(() => {
+					const list = []
 					const table = document.querySelectorAll('.search-contact-card')
 					table.forEach((el) => {
 						let name = el.querySelector('.listing-name')
 						let website = el.querySelector('.contact-url') || ' '
 						let email = el.querySelector('.contact-email') || ' '
 						let phone = el.querySelector('.click-to-call') || ' '
-						scrapedData.list.push([`"${name.innerHTML}", "${website.href || 'Not Found'}", "${email.title || 'Not Found'}", "${phone.href || 'Not Found'}"`])
+						list.push([`"${name.innerHTML}", "${website.href || 'Not Found'}", "${email.title || 'Not Found'}", "${phone.href || 'Not Found'}"`])
 						})
-					return scrapedData
+					return list
 				})
+				
 				await page.goto(`https://www.yellowpages.com.au/search/listings?clue=${businessType}&locationClue=${postCode}&pageNumber=${pageN}&referredBy=www.yellowpages.com.au&selectedViewMode=list`, {
 					waitUntil: 'load'
 				});
+
 				if(pageN < 5) {
 					pageN++;
-					return await test(pageN)
-				} else {
-					return grabData	
-				}
-			
+					scrapedData.list.push(eachPage)
+					return await test(pageN, scrapedData)
+				} 
+			return
 		}
 
-			const data = await test(pageN);
-		
-			const generateCSV =  data.header + data.list.map(e => e.join(",")).join("\n");
+			await dataFetch(pageN, scrapedData);
+			const data = [].concat(...scrapedData.list)
+			const generateCSV =  scrapedData.header + data.map(e => e.join(",")).join("\n");
 
 			fs.writeFile(`./${businessType + postCode + '--' + Date.now()}.csv`, generateCSV, function(err) {
 			if(err) {
